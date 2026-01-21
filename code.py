809870fd197eea86cf094a5dedb47346dd010f00
pattbyte <prototype3698@gmail.com>
2026-01-21 22:45:08 +0200

    used elif

--- code.py
+++ code.py
@@ -15,17 +15,19 @@ while True:
     rdn = random.randint(1,1000000)
     if rdn < 5:
       print("[???]: 0.0005%")
-    if rdn < 10:
+    elif rdn < 10:
       print("[Mythic]: 0.001%")
-    if rdn < 100:
+    elif rdn < 100:
       print("[Incredible]: 0.01%")
-    if rdn < 1000:
+    elif rdn < 1000:
       print("[Gem]: 0.1%")
-    if rdn < 10000:
+    elif rdn < 10000:
       print("[Rare]: 1%")
-    if rdn < 100000:
+    elif rdn < 100000:
       print("[Uncommon]: 10%")
-    if rdn < 500000:
+    elif rdn < 500000:
       print("[Common]: 50%")
-    if rdn < 750000:
+    elif rdn < 750000:
       print("[Basic]: 75%")
+    elif rdn < 999999:
+      print("[Dogwater] : 99.9%")
