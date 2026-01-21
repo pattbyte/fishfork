7c4fc2a212860b594c4cb5a7bf25e3e0eeec1612
pattbyte <prototype3698@gmail.com>
2026-01-21 22:32:26 +0200

    oh damn the indentation was bad

--- code.py
+++ code.py
@@ -10,8 +10,8 @@ print("Welcome to CRNG!")
 time.sleep(0.25)
 print("Press [enter] to start rolling!")
 while True:
-roll = input()
-if roll == "":
+  roll = input()
+  if roll == "":
   rdn = randint(1,1000000)
   if rdn < 5:
     print("[???]: 0.0005%")
