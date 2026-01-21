e386534ff8af248424828aea219c51943c063049
pattbyte <prototype3698@gmail.com>
2026-01-21 22:36:45 +0200

    bruh did i forget how to code

--- code.py
+++ code.py
@@ -12,20 +12,20 @@ print("Press [enter] to start rolling!")
 while True:
   roll = input()
   if roll == "":
-  rdn = randint(1,1000000)
-  if rdn < 5:
-    print("[???]: 0.0005%")
-  if rdn < 10:
-    print("[Mythic]: 0.001%")
-  if rdn < 100:
-    print("[Incredible]: 0.01%")
-  if rdn < 1000:
-    print("[Gem]: 0.1%")
-  if rdn < 10000:
-    print("[Rare]: 1%")
-  if rdn < 100000:
-    print("[Uncommon]: 10%")
-  if rdn < 500000:
-    print("[Common]: 50%")
-  if rdn < 750000:
-    print("[Basic]: 75%")
+    rdn = randint(1,1000000)
+    if rdn < 5:
+      print("[???]: 0.0005%")
+    if rdn < 10:
+      print("[Mythic]: 0.001%")
+    if rdn < 100:
+      print("[Incredible]: 0.01%")
+    if rdn < 1000:
+      print("[Gem]: 0.1%")
+    if rdn < 10000:
+      print("[Rare]: 1%")
+    if rdn < 100000:
+      print("[Uncommon]: 10%")
+    if rdn < 500000:
+      print("[Common]: 50%")
+    if rdn < 750000:
+      print("[Basic]: 75%")
