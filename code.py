3e5deaee41e51ba722c0a2b72dbb2750b08dfdc3
pattbyte <prototype3698@gmail.com>
2026-01-22 16:37:46 +0200

    removed roll counter, imma add it later probably
    sorry but im a beginner and this was such an issue to deal with

--- code.py
+++ code.py
@@ -1,11 +1,5 @@
 import random
 import time
-
-try:
-  with open("CRNG_rolls","r") as y:
-    roll_counter=int(y.read())
-except FileNotFoundError:
-  roll_counter=0
   
 print("\033[36mCRNG v1.0\033[0m")
 time.sleep(0.5)
@@ -15,16 +9,10 @@ print()
 print("\033[32mWelcome to CRNG!\033[0m")
 time.sleep(1)
 print("Press [enter] to start rolling!")
-print("> Use 'stats' for your statistics.")
 print()
 while True:
   roll = input()
   if roll == "":
-    roll_counter+=1
-    rf = open("CRNG_rolls.txt","w")
-    rf.write(f"{roll_counter}")
-    rfread = open("CRNG_rolls.txt","r")
-    rc = rfread.read()
     time.sleep(0.1)
     rdn = random.randint(1,1000000)
     if rdn < 5:
@@ -45,7 +33,3 @@ while True:
       print("\033[37m[Basic]: 75%\033[0m")
     elif rdn < 999999:
       print("\033[37m[Dogwater] : 99.9%\033[0m")
-  elif roll == "stats":
-    print()
-    print("\033[37m> STATS\033[0m")
-    print("Rolls:",roll_counter)
